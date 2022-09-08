53abdcc18681c22df616f523155f3711e5e27b3b
Max <51315394+maxibick@users.noreply.github.com>
2022-09-08 22:44:10 +0200

    Update sensor.py

--- custom_components/wolf/sensor.py
+++ custom_components/wolf/sensor.py
@@ -6,18 +6,23 @@ import logging
 from homeassistant.const import (
     STATE_UNKNOWN,
     TEMP_CELSIUS,
-    DEVICE_CLASS_TEMPERATURE,
-    DEVICE_CLASS_PRESSURE,
-    PRECISION_TENTHS
+    PRECISION_TENTHS,
 )
 from homeassistant.helpers.entity import Entity
-#from homeassistant.components.wolf import DOMAIN
-from custom_components.wolf import DOMAIN
+from homeassistant.components.sensor import SensorDeviceClass
+from .const import (
+    DEVICE_INFO_MODELL,
+    DEVICE_INFO_SW_VERSION,
+    DOMAIN,
+    DEVICE_INFO,
+    DEVICE_INFO_NAME,
+    DEVICE_INFO_MANUFACTURER,
+)
+
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_platform(hass, config, async_add_entities,
-                               discovery_info=None):
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """
     performs setup of the analog sensors, expects a
     reference to an ism8-adapter via hass.data
@@ -27,19 +32,23 @@ async def async_setup_platform(hass, config, async_add_entities,
 
     for nbr in ism8.get_all_sensors().keys():
         if ism8.get_device(nbr) in discovery_info:
-            if ism8.get_type(nbr) not in ("DPT_Switch",
-                                          "DPT_Bool",
-                                          "DPT_Enable",
-                                          "DPT_OpenClose"
-                                          ):
+            if ism8.get_type(nbr) not in (
+                "DPT_Switch",
+                "DPT_Bool",
+                "DPT_Enable",
+                "DPT_OpenClose",
+                "DPT_HVACContrMode",
+                "DPT_HVACMode",
+                "DPT_DHWMode",
+            ):
                 sensors.append(WolfSensor(ism8, nbr))
     async_add_entities(sensors)
 
 
 class WolfSensor(Entity):
     """Implementation of Wolf Heating System Sensor via ISM8-network adapter
-        dp_nbr represents the unique identifier of the up to 200 different
-        sensors
+    dp_nbr represents the unique identifier of the up to 200 different
+    sensors
     """
 
     def __init__(self, ism8, dp_nbr):
@@ -50,17 +59,17 @@ class WolfSensor(Entity):
         self._unit = ism8.get_unit(dp_nbr)
         self._state = STATE_UNKNOWN
         self._ism8 = ism8
-        _LOGGER.debug('Setup sensor no. %d as %s', self.dp_nbr, self._type)
+        _LOGGER.debug("Setup sensor no. %d as %s", self.dp_nbr, self._type)
 
     @property
     def name(self) -> str:
         """Return the name of this sensor."""
-        return (self._device + "_" + self._name).lower().replace(' ', '_')
+        return (self._device + "_" + self._name).lower().replace(" ", "_")
 
     @property
     def unique_id(self):
-        """Return the id of this sensor."""
-        return self.dp_nbr
+        """Return the unique_id of this sensor."""
+        return "wolf." + self._device.lower().replace(" ", "") + "." + str(self.dp_nbr)
 
     @property
     def state(self):
@@ -73,17 +82,17 @@ class WolfSensor(Entity):
     @property
     def device_class(self) -> str:
         """Return the state of the device."""
-        if self._type == 'DPT_Value_Temp':
-            return DEVICE_CLASS_TEMPERATURE
-        elif self._type == 'DPT_Value_Pres':
-            return DEVICE_CLASS_PRESSURE
+        if self._type == "DPT_Value_Temp":
+            return SensorDeviceClass.TEMPERATURE
+        elif self._type == "DPT_Value_Pres":
+            return SensorDeviceClass.PRESSURE
         else:
-            return ''
+            return ""
 
     @property
     def unit_of_measurement(self) -> str:
         """Return the unit of measurement of this entity."""
-        if self._type == 'DPT_Value_Temp':
+        if self._type == "DPT_Value_Temp":
             return TEMP_CELSIUS
         else:
             return self._unit
@@ -97,3 +106,22 @@ class WolfSensor(Entity):
         """Return state"""
         self._state = self._ism8.read(self.dp_nbr)
         return
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {
+                # Serial numbers are unique identifiers within a specific domain
+                (DOMAIN, self.unique_id)
+            },
+            "name": DEVICE_INFO.get(self._device, ("", "", "", ""))[DEVICE_INFO_NAME],
+            "manufacturer": DEVICE_INFO.get(self._device, ("", "", "", ""))[
+                DEVICE_INFO_MANUFACTURER
+            ],
+            "model": DEVICE_INFO.get(self._device, ("", "", "", ""))[
+                DEVICE_INFO_MODELL
+            ],
+            "sw_version": DEVICE_INFO.get(self._device, ("", "", "", ""))[
+                DEVICE_INFO_SW_VERSION
+            ],
+        }
